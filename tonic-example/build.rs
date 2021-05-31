fb9c6051cff87c104d178900f1b85b0d96b9a6bf
Jacob <jacobbishopxy@gmail.com>
2021-05-31 21:37:24 +0800

    ehn tonic-example - add authorization

--- tonic-example/build.rs
+++ tonic-example/build.rs
@@ -4,4 +4,6 @@ fn main() {
 
     tonic_build::compile_protos("proto/routeguide/route_guide.proto")
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+
+    tonic_build::compile_protos("proto/echo/echo.proto").unwrap();
 }
