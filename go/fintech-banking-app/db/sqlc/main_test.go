5140a4b6457f10652565867d2d83f45d30f53445
Jacob <jacobbishopxy@gmail.com>
2021-06-14 13:47:49 +0800

    mnt FBA - update query sql - add store - test cases

--- go/fintech-banking-app/db/sqlc/main_test.go
+++ go/fintech-banking-app/db/sqlc/main_test.go
@@ -15,16 +15,17 @@ const (
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 // TestMain 函数是位于一个 package 下的所有单元测试的主函数
 func TestMain(m *testing.M) {
-	// 返回数据库连接与错误信息
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
---
+++ go/fintech-banking-app/db/sqlc/store.go
@@ -0,0 +1,91 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{New(db), db}
+}
+
+// 通用的数据库事务函数
+// 接受 context 和回调函数作为入参，启动一个数据库事务。
+// 创建一个新的 Queries 对象，调用回调函数 commit 或 rollback 事务。
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, &sql.TxOptions{})
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err: %v, rb err: %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+}
+
+func (store *Store) TransferTx(
+	ctx context.Context,
+	arg TransferTxParams,
+) (TransferTxResult, error) {
+
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+
+		return nil
+
+		// todo: 更新 account balance (involves locking and preventing potential deadlock)
+	})
+
+	return result, err
+}
