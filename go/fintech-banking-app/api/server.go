a8588d88f61b06c637898a30998720189ff67337
Jacob <jacobbishopxy@gmail.com>
2021-06-22 15:38:52 +0800

    mnt go/fintech-banking-app - add api/ - main.go runs a http server

---
+++ go/fintech-banking-app/api/server.go
@@ -0,0 +1,39 @@
+package api
+
+import (
+	db "fintech-banking-app/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+// 定义 server 结构体
+// `db.Store` 即之前所编写的实现。在处理客户端的 API 请求时，它允许我们与进行数据库交互。
+// `gin.Enine` 将帮助我们发送每个 API 请求至正确的函数进行处理加工。
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// 传入一个或多个 handler 函数。
+	// 只有最后的函数为 handler，前面部分的函数皆为 middlewares
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccount)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	// `server.router` 为私有字段，不可被 `api` package 外所访问
+	return server.router.Run(address)
+}
+
+// 接受 error 作为入参，返回 `gin.H` 对象（`map[string]interface{}`的简写）。
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
--- go/fintech-banking-app/go.mod
+++ go/fintech-banking-app/go.mod
@@ -3,6 +3,21 @@ module fintech-banking-app
 go 1.16
 
 require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/go-playground/validator/v10 v10.6.1 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/kr/pretty v0.2.1 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/lib/pq v1.10.2
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/stretchr/testify v1.7.0
+	github.com/ugorji/go v1.2.6 // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	golang.org/x/text v0.3.6 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
