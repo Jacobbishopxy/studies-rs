cd50d96f76d87f159c4140acfdcdd24d71b155a7
Jacob <jacobbishopxy@gmail.com>
2022-05-07 10:02:07 +0800

    chore: frontend

--- web-wasm-demo/server/src/main.rs
+++ web-wasm-demo/server/src/main.rs
@@ -1,6 +1,6 @@
 //! Server
 //!
-//!  
+//! backend server for web-wasm-demo
 
 use std::{
     net::{IpAddr, Ipv6Addr, SocketAddr},
@@ -9,10 +9,14 @@ use std::{
 
 use axum::{response::IntoResponse, routing, Router};
 use clap::Parser;
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 
 #[derive(Parser, Debug)]
 #[clap(name = "server", about = "A server for our wasm project")]
 struct Opt {
+    #[clap(short = 'l', long = "log", default_value = "debug")]
+    log_level: String,
     #[clap(short = 'a', long = "addr", default_value = "::1")]
     addr: String,
     #[clap(short = 'p', long = "port", default_value = "8080")]
@@ -27,14 +31,23 @@ async fn hello() -> impl IntoResponse {
 async fn main() {
     let opt = Opt::parse();
 
-    let app = Router::new().route("/", routing::get(hello));
+    // 配置日志以及 RUST_LOG
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", format!("{},hyper=info,mio=info", opt.log_level));
+    }
+    // 启用打印
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/", routing::get(hello))
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
 
     let sock_addr = SocketAddr::from((
         IpAddr::from_str(opt.addr.as_str()).unwrap_or(IpAddr::V6(Ipv6Addr::LOCALHOST)),
         opt.port,
     ));
 
-    println!("listening on http://{sock_addr}");
+    log::info!("listening on http://{sock_addr}");
 
     axum::Server::bind(&sock_addr)
         .serve(app.into_make_service())
